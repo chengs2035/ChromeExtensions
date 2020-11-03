775d60cc248bce49654a6c2ae20e130de96f923a
djc8 <chensongguo@gmail.com>
2020-11-03 18:19:11 +0800

    调整目录结构，添加文件

---
+++ swagger_server/swagger2_9_2/src/main/java/cn/djc8/chrome/swagger2_9_2/Swagger292Application.java
@@ -0,0 +1,13 @@
+package cn.djc8.chrome.swagger2_9_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger292Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swagger292Application.class, args);
+	}
+
+}
