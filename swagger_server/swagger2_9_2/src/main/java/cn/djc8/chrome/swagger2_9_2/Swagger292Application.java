ef3200edf417315717bd90b2f7128d4d3a560d1d
djc8 <chensongguo@gmail.com>
2020-11-03 18:39:50 +0800

    增加2_9_2界面显示

--- swagger_server/swagger2_9_2/src/main/java/cn/djc8/chrome/swagger2_9_2/Swagger292Application.java
+++ swagger_server/swagger2_9_2/src/main/java/cn/djc8/chrome/swagger2_9_2/Swagger292Application.java
@@ -2,8 +2,10 @@ package cn.djc8.chrome.swagger2_9_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Swagger292Application {
 
 	public static void main(String[] args) {
